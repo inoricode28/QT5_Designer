252ac1be912b588eee147b51086a6cc74eb8a434
inoricode28 <inori237@gmail.com>
2022-11-02 00:15:41 -0500

    MVC05

--- MVC/vista/ui_botones.py
+++ MVC/vista/ui_botones.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'botonesEinRic.ui'
+## Form generated from reading UI file 'botonesKasCha.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -16,39 +16,46 @@ from PySide2.QtWidgets import *
 from Custom_Widgets.Widgets import *
 ########################################################################
 
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(748, 600)
+        MainWindow.resize(837, 427)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.pushButton = QCustomQPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(240, 60, 251, 91))
+        self.pushButton.setGeometry(QRect(10, 50, 251, 91))
         self.pushButton_2 = QCustomQPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(240, 160, 251, 91))
+        self.pushButton_2.setGeometry(QRect(270, 50, 251, 91))
         self.pushButton_3 = QCustomQPushButton(self.centralwidget)
         self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(240, 260, 251, 91))
+        self.pushButton_3.setGeometry(QRect(530, 50, 251, 91))
         self.pushButton_4 = QCustomQPushButton(self.centralwidget)
         self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(240, 360, 251, 91))
+        self.pushButton_4.setGeometry(QRect(10, 150, 251, 91))
         self.pushButton_5 = QCustomQPushButton(self.centralwidget)
         self.pushButton_5.setObjectName(u"pushButton_5")
-        self.pushButton_5.setGeometry(QRect(240, 460, 251, 91))
+        self.pushButton_5.setGeometry(QRect(270, 150, 251, 91))
         self.pushButton_6 = QCustomQPushButton(self.centralwidget)
         self.pushButton_6.setObjectName(u"pushButton_6")
-        self.pushButton_6.setGeometry(QRect(30, 390, 151, 141))
+        self.pushButton_6.setGeometry(QRect(530, 150, 251, 91))
         font = QFont()
         font.setPointSize(36)
         self.pushButton_6.setFont(font)
         self.pushButton_7 = QCustomQPushButton(self.centralwidget)
         self.pushButton_7.setObjectName(u"pushButton_7")
-        self.pushButton_7.setGeometry(QRect(520, 70, 181, 141))
+        self.pushButton_7.setGeometry(QRect(10, 250, 251, 101))
         self.pushButton_7.setFont(font)
+        self.pushButton_8 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_8.setObjectName(u"pushButton_8")
+        self.pushButton_8.setGeometry(QRect(270, 250, 251, 101))
+        self.pushButton_8.setFont(font)
+        self.pushButton_9 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        self.pushButton_9.setGeometry(QRect(530, 250, 251, 101))
+        self.pushButton_9.setFont(font)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -63,7 +70,9 @@ class Ui_MainWindow(object):
         self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
-        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"CLARK", None))
-        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"Alfonzo", None))
+        self.pushButton_6.setText("")
+        self.pushButton_7.setText("")
+        self.pushButton_8.setText("")
+        self.pushButton_9.setText("")
     # retranslateUi
 
