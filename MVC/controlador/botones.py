4635674b3325191475564ed4e0021205cace32a0
inoricode28 <inori237@gmail.com>
2022-11-02 00:08:17 -0500

    MVC04

--- MVC/controlador/botones.py
+++ MVC/controlador/botones.py
@@ -1,7 +1,11 @@
 from  PySide2 import QtWidgets , QtCore
 import sys
+import iconify as ico
+from iconify.qt import QtGui, QtWidgets, QtCore
+from Custom_Widgets.Widgets import *
 #Vista.ui_botones eso signidica Carpeta.archivo
-from vista.ui_botones import Ui_MainWindow #Ui_MainWindow es la clase que se encuentra en la particula
+#from vista.ui_botones import Ui_MainWindow #Ui_MainWindow es la clase que se encuentra en la particula
+from vista.ui_botones import *
 #El nombre de la clase es cambiable
 #Driver el controlador
 class Clark(QtWidgets.QMainWindow):#Boton es cambiable
@@ -9,12 +13,27 @@ class Clark(QtWidgets.QMainWindow):#Boton es cambiable
     def __init__(self, *args , parent=None):
         
         super(Clark, self).__init__(parent)#Boton es cambiable
-        self.venPri = Ui_MainWindow()# Particula
-        self.venPri.setupUi(self)  
-        self.venPri.pushButton_5.clicked.connect(lambda: self.close())
-        self.venPri.pushButton_6.clicked.connect(self.cerrar)
+        self.ventPrin = Ui_MainWindow()# Particula
+        self.ventPrin.setupUi(self)         
+        #self.ventPrin.pushButton_5.clicked.connect(lambda: self.close())
+        #self.ventPrin.pushButton_6.clicked.connect(self.cerrar)
+        loadJsonStyle(self, self.ventPrin)
+        
+        self.ventPrin.pushButton.setObjectTheme(1)
+        self.ventPrin.pushButton_2.setObjectTheme(2)
+        self.ventPrin.pushButton_3.setObjectTheme(3)
+        self.ventPrin.pushButton_4.setObjectTheme(4)
+        self.ventPrin.pushButton_5.setObjectTheme(5)
+        self.ventPrin.pushButton_6.setObjectTheme(6)
+        self.ventPrin.pushButton_7.setObjectTheme(11)
+        self.ventPrin.pushButton_5.setObjectTheme(10)
+        self.ventPrin.pushButton.setObjectCustomTheme("#fff", "#000")
+        self.ventPrin.pushButton.setObjectAnimateOn("hover")
+        self.ventPrin.pushButton_7.setObjectAnimateOn("click")
+        #self.ui.pushButton._animation.setEasingCurve(QtCore.QEasingCurve.InOutElastic)
+        self.ventPrin.pushButton._animation.setEasingCurve(QtCore.QEasingCurve.InQuad)
     
-    def cerrar(self):
-        self.close()
+    #def cerrar(self):
+        #self.close()
         
     
--- MVC/vista/ui_botones.py
+++ MVC/vista/ui_botones.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'botonescPFgQQ.ui'
+## Form generated from reading UI file 'botonesEinRic.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -11,6 +11,10 @@
 from PySide2.QtCore import *
 from PySide2.QtGui import *
 from PySide2.QtWidgets import *
+########################################################################
+# IMPORT CUSTOM BUTTONS FILE
+from Custom_Widgets.Widgets import *
+########################################################################
 
 
 class Ui_MainWindow(object):
@@ -20,27 +24,31 @@ class Ui_MainWindow(object):
         MainWindow.resize(748, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.pushButton = QPushButton(self.centralwidget)
+        self.pushButton = QCustomQPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(220, 140, 251, 91))
-        self.pushButton_2 = QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QRect(240, 60, 251, 91))
+        self.pushButton_2 = QCustomQPushButton(self.centralwidget)
         self.pushButton_2.setObjectName(u"pushButton_2")
-        self.pushButton_2.setGeometry(QRect(220, 240, 251, 91))
-        self.pushButton_3 = QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QRect(240, 160, 251, 91))
+        self.pushButton_3 = QCustomQPushButton(self.centralwidget)
         self.pushButton_3.setObjectName(u"pushButton_3")
-        self.pushButton_3.setGeometry(QRect(220, 340, 251, 91))
-        self.pushButton_4 = QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QRect(240, 260, 251, 91))
+        self.pushButton_4 = QCustomQPushButton(self.centralwidget)
         self.pushButton_4.setObjectName(u"pushButton_4")
-        self.pushButton_4.setGeometry(QRect(220, 440, 251, 91))
-        self.pushButton_5 = QPushButton(self.centralwidget)
+        self.pushButton_4.setGeometry(QRect(240, 360, 251, 91))
+        self.pushButton_5 = QCustomQPushButton(self.centralwidget)
         self.pushButton_5.setObjectName(u"pushButton_5")
-        self.pushButton_5.setGeometry(QRect(220, 40, 251, 91))
-        self.pushButton_6 = QPushButton(self.centralwidget)
+        self.pushButton_5.setGeometry(QRect(240, 460, 251, 91))
+        self.pushButton_6 = QCustomQPushButton(self.centralwidget)
         self.pushButton_6.setObjectName(u"pushButton_6")
         self.pushButton_6.setGeometry(QRect(30, 390, 151, 141))
         font = QFont()
         font.setPointSize(36)
         self.pushButton_6.setFont(font)
+        self.pushButton_7 = QCustomQPushButton(self.centralwidget)
+        self.pushButton_7.setObjectName(u"pushButton_7")
+        self.pushButton_7.setGeometry(QRect(520, 70, 181, 141))
+        self.pushButton_7.setFont(font)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -56,5 +64,6 @@ class Ui_MainWindow(object):
         self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.pushButton_5.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
         self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"CLARK", None))
+        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"Alfonzo", None))
     # retranslateUi
 
